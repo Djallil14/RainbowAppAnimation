5c60d7d22f8900bad002d45e62d2a42782b7770c
Djallil Elkebir <elkebir.djallil@icloud.com>
2021-07-20 18:51:34 -0400

    Initial Commit

---
+++ RainbowAnimationTest/RainbowAnimationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainbowAnimationTestApp.swift
+//  RainbowAnimationTest
+//
+//  Created by Djallil Elkebir on 2021-07-20.
+//
+
+import SwiftUI
+
+@main
+struct RainbowAnimationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
